4b300f6b88ebbf9c7da7abf290606e250dc37bb0
Madhav Jadav <102902932+jadavmadhavkumar@users.noreply.github.com>
2024-03-07 19:35:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,83 @@
-let boxes = document.querySelectorAll('.box');
-let resetBtn = document.querySelector('#reset-btn');
-let newGameBtn = document.querySelector('#reset-btn');
-let msgContainer = document.querySelector('.msg-container');
-let msg = document.querySelector('#msg');
+document.addEventListener("DOMContentLoaded", () => {
+  const boxes = document.querySelectorAll(".box");
+  const messageContainer = document.querySelector(".mes-conatainer");
+  const message = document.querySelector("#message");
+  const newGameBtn = document.querySelector("#new-btn");
+  const resetBtn = document.querySelector("#reset-btn");
 
+  let currentPlayer = "X";
+  let isGameActive = true;
+  let moves = 0;
 
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
 
-const winPatterns = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+  const handleBoxClick = (e) => {
+    const box = e.target;
+    const boxIndex = Array.from(boxes).indexOf(box);
 
-const resetGame = () => {
+    if (box.innerText === "" && isGameActive) {
+      box.innerText = currentPlayer;
+      moves++;
+
+      if (checkWin(currentPlayer)) {
+        endGame(false);
+        message.innerText = `Player ${currentPlayer} wins!`;
+      } else if (moves === 9) {
+        endGame(true);
+        message.innerText = "It's a draw!";
+      } else {
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        message.innerText = `${currentPlayer}'s turn`;
+      }
+    }
+  };
+
+  const checkWin = (player) => {
+    for (let i = 0; i < winningCombinations.length; i++) {
+      let matchCount = 0;
+      for (let j = 0; j < winningCombinations[i].length; j++) {
+        if (boxes[winningCombinations[i][j]].innerText === player) {
+          matchCount++;
+        }
+      }
+      if (matchCount === 3) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const endGame = (isDraw) => {
+    isGameActive = false;
+    if (isDraw) {
+      message.innerText = "It's a draw!";
+    }
+    messageContainer.classList.remove("hide");
+  };
+
+  const startNewGame = () => {
+    currentPlayer = "X";
+    isGameActive = true;
+    moves = 0;
+    message.innerText = `${currentPlayer}'s turn`;
+    messageContainer.classList.add("hide");
+    boxes.forEach((box) => {
+      box.innerText = "";
+    });
+  };
+
+  resetBtn.addEventListener("click", startNewGame);
+  newGameBtn.addEventListener("click", startNewGame);
   boxes.forEach((box) => {
-    enableBoxes();
-    box.innerText = '';
-    box.disabled = false;
-  });
-  trurn0 = true;
-};
-
-const disabledBoxes = () => {
-    for (let i = 0; i < boxes.length; i++) {
-        boxes[i].disabled = true;
-    }
-};
-
-
-
-boxes.forEach((box) => {
-  box.addEventListener('click', () => {
-    console.log('box clicked');
-    if (trurn0) {
-      box.innerText = 'X';
-      trurn0 = false;
-    }
-    else {
-      box.innerText = 'O';
-      trurn0 = true;
-    }
-    box.disabled = true;
-    checkWinner();
+    box.addEventListener("click", handleBoxClick);
   });
 });
-
-const checkWinner = () => {
-  for (let i = 0; i < winPatterns.length; i++) {
-    let [a, b, c] = winPatterns[i];
-    if (boxes[a].innerText === boxes[b].innerText && boxes[b].innerText === boxes[c].innerText && boxes[a].innerText !== '') {
-      alert(`${boxes[a].innerText} is the winner!`);
-      resetGame();
-      showMsg(`${boxes[a].innerText} is the winner!`);
-      msgContainer.removeAttribute('hide');
-    }
-  }
-}
-
-newGameBtn.addEventListener('click', () => {
-    resetGame();
-    msgContainer.setAttribute('hide', '');
-    });
-
-resetBtn.addEventListener('click', () => {  
-    resetGame();
-    msgContainer.setAttribute('hide', '');
-    }
-);
-
-console.log('Check if all necessary requirements are met');
-console.error('Check for any syntax errors');
-console.warn('Check logic and flow of the code');
